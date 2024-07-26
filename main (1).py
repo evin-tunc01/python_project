a2bcd891ebadbe6dbdd3df7c18e1f74fa074131c
evin-tunc01 <evintunc62@gmail.com>
2024-07-26 16:02:52 +0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,13 @@
+#ZAMLI VE PRİMLİ MAAŞI HESAPLAMA
+maaş=input("maaşınızı giriniz")
+zam=input("zam oranını giriniz(%)")
+prim=input("kaç adet ürün sattığınızı yazın.")
+yenimaas=int(maaş)+int(maaş)*int(zam)/100+int(prim)*900
+print("YENİ MAAŞINIZ:{0}".format(yenimaas))
+#TATİL ÜCRETİ HESAPLAMA
+kişi=input("kişi sayısını giriniz")
+oda=input("oda sayısı yazınız")
+lux=input("kral dairesi ister misiniz (e/h)")
+
+
+
